8c0c17b0b2fdbd0604c6f48a52903410a75c34bf
Sergei Bykov <60148493+SergeiDBykov@users.noreply.github.com>
2024-06-12 18:42:17 +0200

    start classes for tng handling

--- utils/data/tng_query.py
+++ utils/data/tng_query.py
@@ -41,170 +41,170 @@ def get(path, params=None):
 
 
 
-def download_halo_snapshot(subhalo_url, retry = True):
-    try:
-        halo_id = subhalo_url.split('/')[-2]
-        cutout_request = {'dm':'Coordinates,Potential'}
-        cutout_name = f'halo_{halo_id}_cutout.hdf5'
-        filepath = f'{data_path}/{cutout_name}'
+# def download_halo_snapshot(subhalo_url, retry = True):
+#     try:
+#         halo_id = subhalo_url.split('/')[-2]
+#         cutout_request = {'dm':'Coordinates,Potential'}
+#         cutout_name = f'halo_{halo_id}_cutout.hdf5'
+#         filepath = f'{data_path}/{cutout_name}'
 
-        if os.path.exists(filepath):
-            filesize = os.path.getsize(filepath)
-            print(f'{cutout_name} already exists, {filesize/1e6:.2f} MB')
-            return filepath
+#         if os.path.exists(filepath):
+#             filesize = os.path.getsize(filepath)
+#             print(f'{cutout_name} already exists, {filesize/1e6:.2f} MB')
+#             return filepath
         
-        t0 = time.time()
+#         t0 = time.time()
 
-        download_url = subhalo_url + 'cutout.hdf5'
-        print(f'Downloading cutout from {download_url}')
-        cutout = get(download_url, cutout_request)
-        t1 = time.time()
-        filesize = os.path.getsize(cutout)
-        print(f'Downloaded {cutout_name} in {t1-t0:.2f} s, {filesize/1e6:.2f} MB')
-        os.rename(cutout, filepath)
-        return filepath
+#         download_url = subhalo_url + 'cutout.hdf5'
+#         print(f'Downloading cutout from {download_url}')
+#         cutout = get(download_url, cutout_request)
+#         t1 = time.time()
+#         filesize = os.path.getsize(cutout)
+#         print(f'Downloaded {cutout_name} in {t1-t0:.2f} s, {filesize/1e6:.2f} MB')
+#         os.rename(cutout, filepath)
+#         return filepath
 
-    except Exception as e:
-        print(f'Error downloading {subhalo_url}, {e}')
-        if retry:
-            print(f'retrying...')
-            return download_halo_snapshot(subhalo_url, retry = False)
-        else:
-            print(f'stopping')
-            return None
-
-
-
-def make_3d_density(haloid,
-                    box_size = 500,
-                    grid_bins = 128,
-                    smooth_R = 5,
-                    smooth_type = 'Gaussian'
-
-                    ):
-
-    cutout_file = download_halo_snapshot(f'{baseUrl}{base_query}subhalos/{haloid}/')
-    with h5py.File(cutout_file, 'r') as f:
-        x = f['PartType1']['Coordinates'][:,0]
-        y = f['PartType1']['Coordinates'][:,1]
-        z = f['PartType1']['Coordinates'][:,2]
-        pot = f['PartType1']['Potential'][:]
-
-        #coords of max potential
-        max_pot = np.argmin(pot)
-        x_max = x[max_pot]
-        y_max = y[max_pot]
-        z_max = z[max_pot]
-
-        #centering
-        x -= x_max
-        y -= y_max
-        z -= z_max
-
-        pos = np.array([x,y,z]).T
-
-        hist, edges = np.histogramdd(pos, bins = grid_bins, range = [[-box_size/2, box_size/2]]*3)
-
-        hist_32 = hist.astype(np.float32)
-
-        # #pylians3 https://pylians3.readthedocs.io/en/master/construction.html
-
-        W_k = pylians_SL.FT_filter(box_size, smooth_R, grid_bins, smooth_type, 28)
-
-        hist_smoothed_32 = pylians_SL.field_smoothing(hist_32, W_k, 28)
-
-        #hist_smoothed_32[hist_smoothed_32 < 0] = 0
-
-
-    result = {'hist':hist, 'hist_smoothed':hist_smoothed_32, 'edges':edges, 'pos':pos, 'pot':pot,  'haloid':haloid}
-
-    return result
+#     except Exception as e:
+#         print(f'Error downloading {subhalo_url}, {e}')
+#         if retry:
+#             print(f'retrying...')
+#             return download_halo_snapshot(subhalo_url, retry = False)
+#         else:
+#             print(f'stopping')
+#             return None
 
 
 
-def plot_3d_density(dens_res):
-    hist = dens_res['hist']
-    hist_smoothed = dens_res['hist_smoothed']
-    edges = dens_res['edges']
+# def make_3d_density(haloid,
+#                     box_size = 500,
+#                     grid_bins = 128,
+#                     smooth_R = 5,
+#                     smooth_type = 'Gaussian'
 
-    #make 3x2 plots which are sum of 3d density along each axis, and unsmoothed/smoothed
+#                     ):
 
-    fig, axs = plt.subplots(3,2, figsize = (10,15))
+#     cutout_file = download_halo_snapshot(f'{baseUrl}{base_query}subhalos/{haloid}/')
+#     with h5py.File(cutout_file, 'r') as f:
+#         x = f['PartType1']['Coordinates'][:,0]
+#         y = f['PartType1']['Coordinates'][:,1]
+#         z = f['PartType1']['Coordinates'][:,2]
+#         pot = f['PartType1']['Potential'][:]
 
-    for axis_i in range(3):
-        for smooth_i in range(2):
-            ax = axs[axis_i, smooth_i]
-            if smooth_i == 0:
-                map_2d = hist.sum(axis = axis_i).T
-            else:
-                map_2d = hist_smoothed.sum(axis = axis_i).T
+#         #coords of max potential
+#         max_pot = np.argmin(pot)
+#         x_max = x[max_pot]
+#         y_max = y[max_pot]
+#         z_max = z[max_pot]
+
+#         #centering
+#         x -= x_max
+#         y -= y_max
+#         z -= z_max
+
+#         pos = np.array([x,y,z]).T
+
+#         hist, edges = np.histogramdd(pos, bins = grid_bins, range = [[-box_size/2, box_size/2]]*3)
+
+#         hist_32 = hist.astype(np.float32)
+
+#         # #pylians3 https://pylians3.readthedocs.io/en/master/construction.html
+
+#         W_k = pylians_SL.FT_filter(box_size, smooth_R, grid_bins, smooth_type, 28)
+
+#         hist_smoothed_32 = pylians_SL.field_smoothing(hist_32, W_k, 28)
+
+#         #hist_smoothed_32[hist_smoothed_32 < 0] = 0
+
+
+#     result = {'hist':hist, 'hist_smoothed':hist_smoothed_32, 'edges':edges, 'pos':pos, 'pot':pot,  'haloid':haloid}
+
+#     return result
+
+
+
+# def plot_3d_density(dens_res):
+#     hist = dens_res['hist']
+#     hist_smoothed = dens_res['hist_smoothed']
+#     edges = dens_res['edges']
+
+#     #make 3x2 plots which are sum of 3d density along each axis, and unsmoothed/smoothed
+
+#     fig, axs = plt.subplots(3,2, figsize = (10,15))
+
+#     for axis_i in range(3):
+#         for smooth_i in range(2):
+#             ax = axs[axis_i, smooth_i]
+#             if smooth_i == 0:
+#                 map_2d = hist.sum(axis = axis_i).T
+#             else:
+#                 map_2d = hist_smoothed.sum(axis = axis_i).T
             
-            map_2d = -np.log10(map_2d/np.nanmax(map_2d))
+#             map_2d = -np.log10(map_2d/np.nanmax(map_2d))
 
-            im = ax.imshow(map_2d, cmap='bone', norm=LogNorm(vmin=0.01, vmax=5))
-            ax.contour(map_2d, levels=[1,2,3,4,5], cmap='Reds', linewidths=1.5)
+#             im = ax.imshow(map_2d, cmap='bone', norm=LogNorm(vmin=0.01, vmax=5))
+#             ax.contour(map_2d, levels=[1,2,3,4,5], cmap='Reds', linewidths=1.5)
 
-            title_x = ['yz', 'xz', 'xy'][axis_i]
-            title_smooth = ['unsmoothed', 'smoothed'][smooth_i]
-            ax.set_title(f'{title_x} {title_smooth}')
+#             title_x = ['yz', 'xz', 'xy'][axis_i]
+#             title_smooth = ['unsmoothed', 'smoothed'][smooth_i]
+#             ax.set_title(f'{title_x} {title_smooth}')
 
-    #set title
-    fig.suptitle(f'3d density, halo {dens_res["haloid"]}')
-    plt.show()
-
-
-    #make a plot with colorbar and levels of xy plane
-
-    f = figure(figsize=(12, 12))
-    ax = f.add_axes([0.17, 0.02, 0.72, 0.79])
-    axcolor = f.add_axes([0.90, 0.02, 0.03, 0.79])
-
-    map_2d = hist_smoothed.sum(axis = 2).T
-    map_2d = -np.log10(map_2d/np.nanmax(map_2d))
-
-    im = ax.imshow(map_2d, cmap='bone', norm=LogNorm(vmin=0.01, vmax=5))
-    levels = [1,2,3,4]
-    CS = ax.contour(map_2d, levels=levels, cmap='Reds', linewidths=1.5)
-
-    ax.clabel(CS, CS.levels, inline=True, fmt='%1.0f', fontsize=10)
-
-    #f.colorbar(im, cax=axcolor, orientation='vertical')
-    f.colorbar(im, cax=axcolor, orientation='vertical', ticks = levels, label = 'negative  log10 [density/max density]')
-    ax.set_title(f'xy smoothed density, halo {dens_res["haloid"]}')
-    plt.show()
+#     #set title
+#     fig.suptitle(f'3d density, halo {dens_res["haloid"]}')
+#     plt.show()
 
 
+#     #make a plot with colorbar and levels of xy plane
 
-def download_sublink(subhalo_url, retry = True):
-    try:
-        halo_id = subhalo_url.split('/')[-2]
-        mass_hist_name = f'halo_{halo_id}_sublink.hdf5'
-        filepath = f'{data_path}/{mass_hist_name}'
+#     f = figure(figsize=(12, 12))
+#     ax = f.add_axes([0.17, 0.02, 0.72, 0.79])
+#     axcolor = f.add_axes([0.90, 0.02, 0.03, 0.79])
 
-        if os.path.exists(filepath):
-            filesize = os.path.getsize(filepath)
-            print(f'{mass_hist_name} already exists, {filesize/1e6:.2f} MB')
-            return filepath
+#     map_2d = hist_smoothed.sum(axis = 2).T
+#     map_2d = -np.log10(map_2d/np.nanmax(map_2d))
+
+#     im = ax.imshow(map_2d, cmap='bone', norm=LogNorm(vmin=0.01, vmax=5))
+#     levels = [1,2,3,4]
+#     CS = ax.contour(map_2d, levels=levels, cmap='Reds', linewidths=1.5)
+
+#     ax.clabel(CS, CS.levels, inline=True, fmt='%1.0f', fontsize=10)
+
+#     #f.colorbar(im, cax=axcolor, orientation='vertical')
+#     f.colorbar(im, cax=axcolor, orientation='vertical', ticks = levels, label = 'negative  log10 [density/max density]')
+#     ax.set_title(f'xy smoothed density, halo {dens_res["haloid"]}')
+#     plt.show()
+
+
+
+# def download_sublink(subhalo_url, retry = True):
+#     try:
+#         halo_id = subhalo_url.split('/')[-2]
+#         mass_hist_name = f'halo_{halo_id}_sublink.hdf5'
+#         filepath = f'{data_path}/{mass_hist_name}'
+
+#         if os.path.exists(filepath):
+#             filesize = os.path.getsize(filepath)
+#             print(f'{mass_hist_name} already exists, {filesize/1e6:.2f} MB')
+#             return filepath
         
-        t0 = time.time()
+#         t0 = time.time()
 
-        download_url = subhalo_url + 'sublink/full.hdf5'
-        print(f'Downloading cutout from {download_url}')
-        sublink = get(download_url)
-        t1 = time.time()
-        filesize = os.path.getsize(sublink)
-        print(f'Downloaded {mass_hist_name} in {t1-t0:.2f} s, {filesize/1e6:.2f} MB')
-        os.rename(sublink, filepath)
-        return filepath
+#         download_url = subhalo_url + 'sublink/full.hdf5'
+#         print(f'Downloading cutout from {download_url}')
+#         sublink = get(download_url)
+#         t1 = time.time()
+#         filesize = os.path.getsize(sublink)
+#         print(f'Downloaded {mass_hist_name} in {t1-t0:.2f} s, {filesize/1e6:.2f} MB')
+#         os.rename(sublink, filepath)
+#         return filepath
 
-    except Exception as e:
-        print(f'Error downloading sublink {subhalo_url}, {e}')
-        if retry:
-            print(f'retrying...')
-            return download_sublink(subhalo_url, retry = False)
-        else:
-            print(f'stopping')
-            return None
+#     except Exception as e:
+#         print(f'Error downloading sublink {subhalo_url}, {e}')
+#         if retry:
+#             print(f'retrying...')
+#             return download_sublink(subhalo_url, retry = False)
+#         else:
+#             print(f'stopping')
+#             return None
 
 
 
@@ -267,7 +267,193 @@ def download_sublink(subhalo_url, retry = True):
 
 
 
-class Halo:
+class HaloInfo:
     def __init__(self, haloid):
         self.haloid = haloid
+
+        self.halo_url = f'{baseUrl}{base_query}subhalos/{haloid}/'
+
+        self.cutout_url = f'{self.halo_url}cutout.hdf5'
+        self.sublink_url = f'{self.halo_url}sublink/full.hdf5'
+
+        self.cutout_file = data_path + f'/halo_{haloid}_cutout.hdf5'
+        self.sublink_file = data_path + f'/halo_{haloid}_sublink.hdf5'
+
+
+    
+    #make download_halo_snapshot but as a method
+    def download_halo_snapshot(self, retry = True):
+        subhalo_url = self.halo_url
+        try:
+            halo_id = subhalo_url.split('/')[-2]
+            cutout_request = {'dm':'Coordinates,Potential'}
+            cutout_name = f'halo_{halo_id}_cutout.hdf5'
+            filepath = f'{data_path}/{cutout_name}'
+
+            if os.path.exists(filepath):
+                filesize = os.path.getsize(filepath)
+                print(f'{cutout_name} already exists, {filesize/1e6:.2f} MB')
+                return filepath
+            
+            t0 = time.time()
+
+            download_url = subhalo_url + 'cutout.hdf5'
+            print(f'Downloading cutout from {download_url}')
+            cutout = get(download_url, cutout_request)
+            t1 = time.time()
+            filesize = os.path.getsize(cutout)
+            print(f'Downloaded {cutout_name} in {t1-t0:.2f} s, {filesize/1e6:.2f} MB')
+            os.rename(cutout, filepath)
+            return filepath
+
+        except Exception as e:
+            print(f'Error downloading {subhalo_url}, {e}')
+            if retry:
+                print(f'retrying...')
+                return self.download_halo_snapshot(retry = False)
+            else:
+                print(f'stopping')
+                return None
+
+
+    def download_sublink(self, retry = True):
+        subhalo_url = self.halo_url
+
+        try:
+            halo_id = subhalo_url.split('/')[-2]
+            mass_hist_name = f'halo_{halo_id}_sublink.hdf5'
+            filepath = f'{data_path}/{mass_hist_name}'
+
+            if os.path.exists(filepath):
+                filesize = os.path.getsize(filepath)
+                print(f'{mass_hist_name} already exists, {filesize/1e6:.2f} MB')
+                return filepath
+            
+            t0 = time.time()
+
+            download_url = subhalo_url + 'sublink/full.hdf5'
+            print(f'Downloading cutout from {download_url}')
+            sublink = get(download_url)
+            t1 = time.time()
+            filesize = os.path.getsize(sublink)
+            print(f'Downloaded {mass_hist_name} in {t1-t0:.2f} s, {filesize/1e6:.2f} MB')
+            os.rename(sublink, filepath)
+            return filepath
+
+        except Exception as e:
+            print(f'Error downloading sublink {subhalo_url}, {e}')
+            if retry:
+                print(f'retrying...')
+                return self.download_sublink(retry = False)
+            else:
+                print(f'stopping')
+                return None
+
+
+    def make_3d_density(self,
+                        box_size = 500,
+                        grid_bins = 128,
+                        smooth_R = 5,
+                        smooth_type = 'Gaussian'
+
+                        ):
+        haloid = self.haloid
+
+        cutout_file = self.cutout_file
+
+        with h5py.File(cutout_file, 'r') as f:
+            x = f['PartType1']['Coordinates'][:,0]
+            y = f['PartType1']['Coordinates'][:,1]
+            z = f['PartType1']['Coordinates'][:,2]
+            pot = f['PartType1']['Potential'][:]
+
+            #coords of max potential
+            max_pot = np.argmin(pot)
+            x_max = x[max_pot]
+            y_max = y[max_pot]
+            z_max = z[max_pot]
+
+            #centering
+            x -= x_max
+            y -= y_max
+            z -= z_max
+
+            pos = np.array([x,y,z]).T
+
+            hist, edges = np.histogramdd(pos, bins = grid_bins, range = [[-box_size/2, box_size/2]]*3)
+
+            hist_32 = hist.astype(np.float32)
+
+            # #pylians3 https://pylians3.readthedocs.io/en/master/construction.html
+
+            W_k = pylians_SL.FT_filter(box_size, smooth_R, grid_bins, smooth_type, 28)
+
+            hist_smoothed_32 = pylians_SL.field_smoothing(hist_32, W_k, 28)
+
+            #hist_smoothed_32[hist_smoothed_32 < 0] = 0
+
+
+        result = {'hist':hist, 'hist_smoothed':hist_smoothed_32, 'edges':edges, 'pos':pos, 'pot':pot,  'haloid':haloid}
+
+        return result
+
+
+
+    @staticmethod
+    def _plot_3d_density(dens_res):
+        hist = dens_res['hist']
+        hist_smoothed = dens_res['hist_smoothed']
+        edges = dens_res['edges']
+
+        #make 3x2 plots which are sum of 3d density along each axis, and unsmoothed/smoothed
+
+        fig, axs = plt.subplots(3,2, figsize = (10,15))
+
+        for axis_i in range(3):
+            for smooth_i in range(2):
+                ax = axs[axis_i, smooth_i]
+                if smooth_i == 0:
+                    map_2d = hist.sum(axis = axis_i).T
+                else:
+                    map_2d = hist_smoothed.sum(axis = axis_i).T
+                
+                map_2d = -np.log10(map_2d/np.nanmax(map_2d))
+
+                im = ax.imshow(map_2d, cmap='bone', norm=LogNorm(vmin=0.01, vmax=5))
+                ax.contour(map_2d, levels=[1,2,3,4,5], cmap='Reds', linewidths=1.5)
+
+                title_x = ['yz', 'xz', 'xy'][axis_i]
+                title_smooth = ['unsmoothed', 'smoothed'][smooth_i]
+                ax.set_title(f'{title_x} {title_smooth}')
+
+        #set title
+        fig.suptitle(f'3d density, halo {dens_res["haloid"]}')
+        plt.show()
+
+
+        #make a plot with colorbar and levels of xy plane
+
+        f = figure(figsize=(12, 12))
+        ax = f.add_axes([0.17, 0.02, 0.72, 0.79])
+        axcolor = f.add_axes([0.90, 0.02, 0.03, 0.79])
+
+        map_2d = hist_smoothed.sum(axis = 2).T
+        map_2d = -np.log10(map_2d/np.nanmax(map_2d))
+
+        im = ax.imshow(map_2d, cmap='bone', norm=LogNorm(vmin=0.01, vmax=5))
+        levels = [1,2,3,4]
+        CS = ax.contour(map_2d, levels=levels, cmap='Reds', linewidths=1.5)
+
+        ax.clabel(CS, CS.levels, inline=True, fmt='%1.0f', fontsize=10)
+
+        #f.colorbar(im, cax=axcolor, orientation='vertical')
+        f.colorbar(im, cax=axcolor, orientation='vertical', ticks = levels, label = 'negative  log10 [density/max density]')
+        ax.set_title(f'xy smoothed density, halo {dens_res["haloid"]}')
+        plt.show()
+
+
+    def plot_density(self):
+        dens_res = self.make_3d_density()
+        self._plot_3d_density(dens_res)
+
     
