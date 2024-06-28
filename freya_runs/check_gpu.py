824c778e5de5e891dea50641b6c3d1883eb4d254
SergeiDBykov <sergei.d.bykov@gmail.com>
2024-06-28 17:43:56 +0200

    dry gpu runs

--- freya_runs/check_gpu.py
+++ freya_runs/check_gpu.py
@@ -1,10 +1,25 @@
 import torch
 import time
+import os 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print('device: ', device)
-if device=='cuda':
-    print(torch.cuda.get_device_properties(0).name)
+if 'freya' in os.uname().nodename:
+    print('working of Freya:', os.uname().nodename)
+
+def check_cuda():
+    # Check if CUDA is available
+    cuda_available = torch.cuda.is_available()
+
+    if cuda_available:
+        num_gpus = torch.cuda.device_count()
+        gpu_names = [torch.cuda.get_device_name(i) for i in range(num_gpus)]
+        print(f"CUDA is available. Number of GPUs: {num_gpus}")
+        for idx, name in enumerate(gpu_names):
+            print(f"GPU {idx}: {name}")
+    else:
+        print("CUDA is not available.")
+
+check_cuda()
+
 
 time.sleep(5)
 print('finished')
